fc248ec4f33f4ac7cfc53cb956ed126f1e108d95
zhangwei <18103413327@163.com>
2018-03-22 21:57:55 +0800

    静态资源地址

--- config/index.js
+++ config/index.js
@@ -57,7 +57,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '//s.bujiangjiu.org/vue-elem/dist/',
+    assetsPublicPath: '//s.bujiangjiu.org/vue-mall/dist/',
 
     /**
      * Source Maps
