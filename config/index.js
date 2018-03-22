1190a3d5e504ba4dd8a87a4e33d5be4cc9644998
zhangwei <18103413327@163.com>
2018-03-23 02:15:26 +0800

    test

--- config/index.js
+++ config/index.js
@@ -57,7 +57,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '//s.bujiangjiu.org/vue-mall/dist/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
